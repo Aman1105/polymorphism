29abf822cb6b7020d8004fd30a33f8f88f17b3ea
Administrator <Administrator@172.16.51.1>
2024-08-13 23:04:58 -0700

    first commit

---
+++ src/testMethod.java
@@ -0,0 +1,23 @@
+
+public class testMethod {
+	
+	static void add (double d, int a) {
+		System.out.println("argument type (int,double)");
+	}
+	
+	static void add1 ( double d, int a) {
+		System.out.println("argument type (int,double");
+	}
+	
+	static void add ( double d, double a) {
+		System.out.println("argument type (double,double");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
